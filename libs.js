8f184827b1ceb4ef97f6a5a14b1a1cc0012aec90
Ulfasaar <weyers.ryan@gmail.com>
2019-04-27 19:42:15 +1200

    simplified code and ui, added shortcuts for saving and toggling vim

---
+++ libs.js
@@ -0,0 +1,7 @@
+// My own libs for saving files etc
+
+function save_file(file_name, text){
+    alert("called with file " + file_name);
+    // take the file_name and text send to Python back end which
+    // saves it to disk
+}
